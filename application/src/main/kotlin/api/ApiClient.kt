8e7826f693d78e0173df791ef16159da6909e355
starfy84 <dereck.tu@hotmail.com>
2023-10-17 02:11:07 -0400

    typo

--- application/src/main/kotlin/api/ApiClient.kt
+++ application/src/main/kotlin/api/ApiClient.kt
@@ -6,7 +6,7 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*\
+import io.ktor.serialization.kotlinx.json.*
 import org.opus.models.Todo
 
 class ApiClient {
